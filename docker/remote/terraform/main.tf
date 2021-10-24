197b030db4dc4837ba0f18785606932386be0511
sc0Vu <alk03073135@gmail.com>
2021-10-24 12:13:40 +0800

    Add terraform vars KSERVERS/MSERVERS/PSERVERS

--- docker/remote/terraform/main.tf
+++ docker/remote/terraform/main.tf
@@ -27,6 +27,21 @@ variable "INSTANCE_SIZE" {
   default = "s-1vcpu-1gb"
 }
 
+variable "KSERVERS" {
+  description = "Number of katzenint nodes in testnet"
+  default = "3"
+}
+
+variable "MSERVERS" {
+  description = "Number of mix nodes in testnet"
+  default = "3"
+}
+
+variable "PSERVERS" {
+  description = "Number of provider nodes in testnet"
+  default = "2"
+}
+
 provider "digitalocean" {
   token = "${var.DO_API_TOKEN}"
 }
@@ -36,6 +51,9 @@ module "cluster" {
   name             = "${var.TESTNET_NAME}"
   ssh_key          = "${var.SSH_KEY_FILE}"
   instance_size    = "${var.INSTANCE_SIZE}"
+  kservers         = "${var.KSERVERS}"
+  mservers         = "${var.MSERVERS}"
+  pservers         = "${var.PSERVERS}"
 }
 
 
